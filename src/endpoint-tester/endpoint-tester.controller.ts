7395af0b3f98e819378c7067ea0fba41cce27dc6
Enkela Bregu <60481274+enkelabregu@users.noreply.github.com>
2024-11-20 21:41:10 +0100

    added possiblity to upload docs file on existing endpoint (#2)

--- src/endpoint-tester/endpoint-tester.controller.ts
+++ src/endpoint-tester/endpoint-tester.controller.ts
@@ -1,8 +1,9 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Param, Post } from '@nestjs/common';
-import { ApiBadRequestResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Controller, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiBody, ApiConsumes, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { EndpointTesterService } from './endpoint-tester.service';
 import { FileUploadtDto } from './dto/endpoint-tester.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @ApiTags('Endpoint Tester')
 @Controller('endpoint-tester')
@@ -10,12 +11,34 @@ export class EndpointTesterController {
   constructor(private endpointTesterService: EndpointTesterService){}
 
 
-    @Post()
-    @ApiOperation({summary: "List all possible e2e scenarious for agiven endpoint"})
-    @ApiBadRequestResponse({description: "Invalid data provided."})
-    @ApiOkResponse({description: 'OK', type: String, isArray: true})
-    generateScenarious(@Param() params: FileUploadtDto){
-       console.log(params)
-      return this.endpointTesterService.generateScenarious()
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiOperation({ summary: "List all possible e2e scenarios for a given endpoint" })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'Upload a YAML file',
+    type: 'multipart/form-data',
+    required: true,
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @ApiBadRequestResponse({ description: "Invalid data provided." })
+  @ApiOkResponse({ description: 'OK', type: String, isArray: true })
+  generateScenarios(
+    @Param() params: FileUploadtDto,
+    @UploadedFile() file: Express.Multer.File
+  ) {
+    if (file.mimetype !== 'application/x-yaml' && file.mimetype !== 'text/yaml') {
+      throw new Error('Only .yml files are allowed');
     }
+    console.log(file);
+  }
+  
 }
